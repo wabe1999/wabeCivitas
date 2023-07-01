9878cae5d7f7fa23c8f44d3a963486bcab3f86b5
Erwan Saputra <ramsissquad1999@gmail.com>
2023-07-01 12:16:54 +0800

    initial commit

---
+++ components/script.js
@@ -0,0 +1,8 @@
+function sapa() {
+    let name = prompt("Masukkan nama kamu: ");
+    
+    if (name != null && name != "") {
+      alert("Halo " + name + ", Selamat datang di Website saya!");
+    }
+  }
+  
